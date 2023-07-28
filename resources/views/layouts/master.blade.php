8b7fd1cc0e493a44a4113cac22326d54651c6d8c
Belal Khan <belalkhan.dev@gmail.com>
2023-07-29 00:15:31 +0600

    how it work section design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -161,7 +161,58 @@
                         <h4 class="sub-title">Our Process</h4>
                         <h2 class="title">How it Works</h2>
                     </div>
-                    <div class="how-work-items"></div>
+                    <div class="how-work-items">
+                        <div class="how-work-item">
+                            <div class="icon">
+                                <img src="{{ asset('assets/images/hw-icon/1.png') }}" alt="">
+                                <span class="label">1</span>
+                            </div>
+                            <div class="content">
+                                <h5>Concept</h5>
+                                <p>Analyze the assigned work & prepare goal to be achieved</p>
+                            </div>
+                        </div>
+                        <div class="how-work-item">
+                            <div class="icon">
+                                <img src="{{ asset('assets/images/hw-icon/2.png') }}" alt="">
+                                <span class="label">2</span>
+                            </div>
+                            <div class="content">
+                                <h5>Design Process</h5>
+                                <p>Prepare design solution targeting the goal</p>
+                            </div>
+                        </div>
+                        <div class="how-work-item">
+                            <div class="icon">
+                                <img src="{{ asset('assets/images/hw-icon/3.png') }}" alt="">
+                                <span class="label">3</span>
+                            </div>
+                            <div class="content">
+                                <h5>Supervision</h5>
+                                <p>Analyze the assigned work & prepare goal to be achieved</p>
+                            </div>
+                        </div>
+                        <div class="how-work-item">
+                            <div class="icon">
+                                <img src="{{ asset('assets/images/hw-icon/4.png') }}" alt="">
+                                <span class="label">4</span>
+                            </div>
+                            <div class="content">
+                                <h5>Budget Planning</h5>
+                                <p>Analyze the assigned work & prepare goal to be achieved</p>
+                            </div>
+                        </div>
+                        <div class="how-work-item">
+                            <div class="icon">
+                                <img src="{{ asset('assets/images/hw-icon/5.png') }}" alt="">
+                                <span class="label">5</span>
+                            </div>
+                            <div class="content">
+                                <h5>Execute Deliver</h5>
+                                <p>Execute the work with according to the strategic plan</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
